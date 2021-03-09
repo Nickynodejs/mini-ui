d9aa56478057a83f05f63a0e1d25279ad6c9bd6d
zhangzhao01 <zhangzhao01@zuche.com>
2021-03-09 17:15:15 +0800

    新项目

---
+++ packages/miniapp-ui/lib/es/components/Demo/index.js
@@ -0,0 +1,1 @@
+import e from"react";import{View as t}from"@tarojs/components";import"./index.css";export default()=>e.createElement(t,{className:"demo"},e.createElement(t,{className:"demo-text"},"11111222"));
---
+++ packages/miniapp-ui/lib/es/index.js
@@ -0,0 +1,1 @@
+export{default as Demo}from"./components/Demo/index.js";export{default as Switch}from"./components/Switch/index.js";
